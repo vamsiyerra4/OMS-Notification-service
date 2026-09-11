f6cc3f17c006daefe5ea29fc4f83b318a21f0bb8
vamsi <vamsiyerra42@gmail.com>
2026-09-11 09:24:14 +0530

    Phase 3 - Step 4 - Established a correlationIDS

--- src/main/java/com/orderms/notification_service/kafka/OrderEventConsumer.java
+++ src/main/java/com/orderms/notification_service/kafka/OrderEventConsumer.java
@@ -2,28 +2,61 @@ package com.orderms.notification_service.kafka;
 
 import com.orderms.notification_service.event.OrderPlacedEvent;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.MDC;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 @Service
 @Slf4j
 public class OrderEventConsumer {
 
+    private static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
+
     @KafkaListener(
             topics = "order-events",
             groupId = "notification-service-group"
     )
-    public void consumerOrderPlacedEvent(OrderPlacedEvent event){
-
-        log.info("Order Placed Event Received - Order Id : {}, user Id : {}, product Id : {}, status: {}, timestamp: {}",
-                event.orderId(), event.userId(), event.productId(), event.status(), event.timeStamp());
 
 
+    public void consumerOrderPlacedEvent(OrderPlacedEvent event,
+                                         ConsumerRecord<String, OrderPlacedEvent> record) {
 
-        // Simulated notification — real email/SMS integration comes later
-        log.info("Email sent to user {} for order {}", event.userId(), event.orderId());
+        log.info(">>> NOTIFICATION CONSUMER RECEIVED RECORD: topic={}, partition={}, offset={}",
+                record.topic(),
+                record.partition(),
+                record.offset());
+
+        String correlationId =null;
+        var header = record.headers().lastHeader(CORRELATION_ID_HEADER);
+
+        if(header != null){
+            correlationId = new String(header.value(), StandardCharsets.UTF_8);
+        }
+
+        if(correlationId == null || correlationId.isBlank()) {
+            correlationId = "missing-correlation-id";
+        }
+
+        try {
+            MDC.put("correlationId", correlationId);
+
+            log.info(">>> NOTIFICATION RECEIVED CORRELATION ID: {}", correlationId);
+
+            log.info("Order Placed Event Received - Order Id : {}, user Id : {}, product Id : {}, status: {}, timestamp: {}",
+                    event.orderId(), event.userId(), event.productId(), event.status(), event.timeStamp());
+
+
+            // Simulated notification — real email/SMS integration comes later
+            log.info("Email sent to user {} for order {}", event.userId(), event.orderId());
 
 //        throw new RuntimeException("Test DLT");
+        }finally {
+            MDC.remove("correlationId");
+        }
 
     }
 }
